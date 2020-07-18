6b217eb942c16c3027ca3d4d98e1aed987453d10
陈晓斌 <465120558@qq.com>
2020-07-18 18:17:49 +0800

    feat:新增图片放大查看组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <ImgLooking url="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import HelloWorld from './components/HelloWorld.vue';
+  import ImgLooking from './components/ImgLooking/index.vue';
+  export default {
+    name: 'App',
+    data() {
+      return {
+      }
+    },
+    components: {
+      HelloWorld,
+      ImgLooking,
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/ImgLooking/index.vue
@@ -0,0 +1,47 @@
+<!-- 图片放大查看组件 -->
+<template>
+    <div>
+        <img class="small-img" :src="url" @click="previewImg" />
+        <el-dialog title="提示" :visible.sync="centerDialogVisible" width="35%">
+            <img class="large-img" :src="url" />
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            url: {
+                type: String,
+                default: '',
+            }
+        },
+        data() {
+            return {
+                //url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+                centerDialogVisible: false,
+            }
+        },
+        //生命周期 - 创建完成（访问当前this实例）
+        created() {},
+        //生命周期 - 挂载完成（访问DOM元素）
+        mounted() {},
+        methods: {
+            previewImg() {
+                this.centerDialogVisible = true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /* @import url(); 引入css类 */
+    .small-img {
+        width: 50px;
+        height: 50px;
+    }
+    .large-img {
+        width: 400px;
+        height: 400px;
+    }
+</style>
